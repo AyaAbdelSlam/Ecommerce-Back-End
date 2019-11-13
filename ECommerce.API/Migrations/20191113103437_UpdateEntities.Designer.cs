088f011071c1a809f22b214fb0befe28f4d39e60
Aya AbdulSlam Hemaid <aya.abdel.slam93@gmail.com>
2019-11-13 13:16:49 +0200

    Add Database Migration

--- ECommerce.API/Migrations/20191113103437_UpdateEntities.Designer.cs
+++ ECommerce.API/Migrations/20191113103437_UpdateEntities.Designer.cs
@@ -4,14 +4,16 @@ using ECommerce.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ECommerce.API.Migrations
 {
     [DbContext(typeof(EShopContext))]
-    partial class EShopContextModelSnapshot : ModelSnapshot
+    [Migration("20191113103437_UpdateEntities")]
+    partial class UpdateEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ECommerce.API/Migrations/20191113103437_UpdateEntities.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ECommerce.API.Migrations
+{
+    public partial class UpdateEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FullDescription",
+                table: "Products",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Images",
+                table: "Products",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Products",
+                nullable: true);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Price",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "CartItems",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FullDescription",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Images",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "CartItems");
+        }
+    }
+}
