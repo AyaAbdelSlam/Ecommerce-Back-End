bdfe021ec8b9e0a35c08c7110f6eab18d63d002d
Aya AbdulSlam Hemaid <aya.abdel.slam93@gmail.com>
2019-11-20 13:28:03 +0200

    Migrations Info after Updating Entities Relation

---
+++ ECommerce.API/Migrations/20191120102934_UsersInitialAdd.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ECommerce.API.Migrations
+{
+    public partial class UsersInitialAdd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "TotalCost",
+                table: "Carts",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Carts",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CartId",
+                table: "CartItems",
+                nullable: true);
+
+            migrationBuilder.AddColumn<float>(
+                name: "ItemCost",
+                table: "CartItems",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "CartItems",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    ModifiedDate = table.Column<DateTime>(nullable: false),
+                    RowVersion = table.Column<byte[]>(rowVersion: true, nullable: true),
+                    FullName = table.Column<string>(nullable: true),
+                    Username = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItems_CartId",
+                table: "CartItems",
+                column: "CartId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItems_ProductId",
+                table: "CartItems",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Carts_CartId",
+                table: "CartItems",
+                column: "CartId",
+                principalTable: "Carts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Products_ProductId",
+                table: "CartItems",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Carts_CartId",
+                table: "CartItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Products_ProductId",
+                table: "CartItems");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CartItems_CartId",
+                table: "CartItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CartItems_ProductId",
+                table: "CartItems");
+
+            migrationBuilder.DropColumn(
+                name: "TotalCost",
+                table: "Carts");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Carts");
+
+            migrationBuilder.DropColumn(
+                name: "CartId",
+                table: "CartItems");
+
+            migrationBuilder.DropColumn(
+                name: "ItemCost",
+                table: "CartItems");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "CartItems");
+        }
+    }
+}
