ddd78375a21213d68aed16ede8ff8fb11471a166
Aya AbdulSlam Hemaid <aya.abdel.slam93@gmail.com>
2019-12-02 15:35:35 +0200

    - Adding CORS to allow Calling the Front-End. - Registering some Dependancies in the DI.

--- ECommerce.API/Startup.cs
+++ ECommerce.API/Startup.cs
@@ -18,51 +18,46 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace ECommerce.API
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace ECommerce.API {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddCors ();
+            services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_2);
 
             //Register Context
-            services.AddDbContext<EShopContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("EShopContext"), b => b.MigrationsAssembly("ECommerce.API")));
+            services.AddDbContext<EShopContext> (options =>
+                options.UseSqlServer (Configuration.GetConnectionString ("EShopContext"), b => b.MigrationsAssembly ("ECommerce.API")));
 
             //Register Repositories
-            services.AddScoped<IRepository<Product>, ProductsRepository>();
-            services.AddScoped<IRepository<User>, UsersRepository>();
-            services.AddScoped<IRepository<Cart>, CartsRepository>();
+            services.AddScoped<IRepository<Product>, ProductsRepository> ();
+            services.AddScoped<IRepository<User>, UsersRepository> ();
+            services.AddScoped<IRepository<Cart>, CartsRepository> ();
 
             //Register Services
-            services.AddScoped<IProductsService, ProductsService>();
-            services.AddScoped<IUsersService, UsersService>();
+            services.AddScoped<IProductsService, ProductsService> ();
+            services.AddScoped<IUsersService, UsersService> ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+            } else {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseHsts ();
             }
 
-            app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseCors (builder =>
+                builder.WithOrigins ("http://localhost:4200"));
+            app.UseHttpsRedirection ();
+            app.UseMvc ();
         }
     }
 }
